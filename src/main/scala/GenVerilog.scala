1ab77dfdfdd7f4b3f5ae1c5dae1028478e349265
algebru <ahmedmensh78@gmail.com>
2024-10-05 16:28:49 +0000

    init

---
+++ src/main/scala/GenVerilog.scala
@@ -0,0 +1,55 @@
+//> using scala "2.13.12"
+//> using dep "org.chipsalliance::chisel:6.5.0"
+//> using plugin "org.chipsalliance:::chisel-plugin:6.5.0"
+//> using options "-unchecked", "-deprecation", "-language:reflectiveCalls", "-feature", "-Xcheckinit", "-Xfatal-warnings", "-Ywarn-dead-code", "-Ymacro-annotations"
+package tech.rocksavage.chiselware.SPI
+
+import _root_.circt.stage.ChiselStage
+import _root_.circt.stage.FirtoolOption
+// third-party imports
+import chisel3._
+
+object Main extends App {
+
+  // ######### Getting Setup #########
+  // setting file output directory
+  var output = System.getProperty("output")
+  if (output == null) output = "out"
+  val outputDir = s"$output/generated"
+
+  val myParams = BaseParams(
+    dataWidth = 32,
+    addrWidth = 32
+  )
+  // if output dir does not exist, make path
+  val javaOutputDir = new java.io.File(outputDir)
+  if (!javaOutputDir.exists) javaOutputDir.mkdirs
+
+  // ######### Set Up Top Module HERE #########
+  val top_name = "SPI.sv"
+  val verilog = ChiselStage.emitSystemVerilog(
+    new SPI(myParams, 1000000, SPIMode.Mode0, SPIRole.Slave),
+    firtoolOpts = Array(
+      "--lowering-options=disallowLocalVariables,disallowPackedArrays",
+      "--disable-all-randomization",
+      "--strip-debug-info",
+      "--split-verilog",
+      s"-o=generated/"
+    )
+  )
+  // ##########################################
+
+  // print verilog to console
+  println(verilog)
+
+  // write verilog to file
+  val writer =
+    new java.io.PrintWriter(new java.io.File(s"$outputDir/$top_name"))
+  writer.write(verilog)
+
+  // close writer
+  writer.close()
+
+  // exit
+  System.exit(0)
+}
---
+++ src/test/scala/SPITest.scala
@@ -0,0 +1,120 @@
+package tech.rocksavage.chiselware.SPI
+
+import org.scalatest.Assertions._
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+import chisel3._
+import chisel3.util._
+import chiseltest._
+import chiseltest.coverage._
+import chiseltest.simulator.VerilatorCFlags
+import firrtl2.options.TargetDirAnnotation
+
+class SPIModuleTest extends AnyFlatSpec with ChiselScalatestTester {
+  behavior of "SPIModule"
+
+  val backendAnnotations = Seq(
+    // VerilatorBackendAnnotation,
+    VerilatorCFlags(Seq("--std=c++17")),
+    WriteFstAnnotation,
+  )
+    val myParams =
+      BaseParams(8, 8)
+
+  // Helper function to run the master-slave transmission
+  def runMasterTransmission(dut: SPI, mode: SPIMode.Type): Unit = {
+    // Write data from APB to SPI
+    dut.io.apb.PWDATA.poke(0xA5.U)
+    dut.io.apb.PSEL.poke(true.B)
+    dut.io.apb.PWRITE.poke(true.B)
+    dut.io.apb.PENABLE.poke(true.B)
+
+    // Check if data is being shifted out correctly on MOSI
+    for (i <- 7 until -1) {
+      dut.clock.step(1)
+      println(s"MOSI: ${dut.io.spi.mosi.peek().litValue}")
+      dut.io.spi.mosi.expect(0xA5.U & (1 << i).U)
+    }
+  }
+
+  // Tests for Mode 0
+  it should "work as an SPI master in Mode0" in {
+    test(new SPI(myParams, 1000000, SPIMode.Mode0, SPIRole.Master)).withAnnotations(backendAnnotations) { dut =>
+      runMasterTransmission(dut, SPIMode.Mode0)
+    }
+  }
+
+  it should "work as an SPI slave in Mode0" in {
+    test(new SPI(myParams, 1000000, SPIMode.Mode0, SPIRole.Slave)).withAnnotations(backendAnnotations) { dut =>
+      for (i <- 0 until 8) {
+        dut.io.spi.mosi.poke((i % 2 == 0).B)
+        dut.io.spi.sclk.poke(false.B)
+        dut.clock.step(1)
+        dut.io.spi.sclk.poke(true.B)
+        dut.clock.step(1)
+      }
+      dut.io.apb.PRDATA.expect(0xaa.U)
+    }
+  }
+
+  // Tests for Mode 1
+  it should "work as an SPI master in Mode1" in {
+    test(new SPI(myParams, 1000000, SPIMode.Mode1, SPIRole.Master)).withAnnotations(backendAnnotations) { dut =>
+      runMasterTransmission(dut, SPIMode.Mode1)
+    }
+  }
+
+  it should "work as an SPI slave in Mode1" in {
+    test(new SPI(myParams, 1000000, SPIMode.Mode1, SPIRole.Slave)).withAnnotations(backendAnnotations) { dut =>
+      for (i <- 0 until 8) {
+        dut.io.spi.mosi.poke((i % 2 == 0).B)
+        dut.io.spi.sclk.poke(true.B)
+        dut.clock.step(1)
+        dut.io.spi.sclk.poke(false.B)
+        dut.clock.step(1)
+      }
+      dut.io.apb.PRDATA.expect(0xaa.U)
+    }
+  }
+
+  // Tests for Mode 2
+  it should "work as an SPI master in Mode2" in {
+    test(new SPI(myParams, 1000000, SPIMode.Mode2, SPIRole.Master)).withAnnotations(backendAnnotations) { dut =>
+      runMasterTransmission(dut, SPIMode.Mode2)
+    }
+  }
+
+  it should "work as an SPI slave in Mode2" in {
+    test(new SPI(myParams, 1000000, SPIMode.Mode2, SPIRole.Slave)).withAnnotations(backendAnnotations) { dut =>
+      for (i <- 0 until 8) {
+        dut.io.spi.mosi.poke((i % 2 == 0).B)
+        dut.io.spi.sclk.poke(false.B)
+        dut.clock.step(1)
+        dut.io.spi.sclk.poke(true.B)
+        dut.clock.step(1)
+      }
+      dut.io.apb.PRDATA.expect(0xaa.U)
+    }
+  }
+
+  // Tests for Mode 3
+  it should "work as an SPI master in Mode3" in {
+    test(new SPI(myParams, 1000000, SPIMode.Mode3, SPIRole.Master)).withAnnotations(backendAnnotations) { dut =>
+      runMasterTransmission(dut, SPIMode.Mode3)
+    }
+  }
+
+  it should "work as an SPI slave in Mode3" in {
+    test(new SPI(myParams, 1000000, SPIMode.Mode3, SPIRole.Slave)).withAnnotations(backendAnnotations) { dut =>
+      for (i <- 0 until 8) {
+        dut.io.spi.mosi.poke((i % 2 == 0).B)
+        dut.io.spi.sclk.poke(true.B)
+        dut.clock.step(1)
+        dut.io.spi.sclk.poke(false.B)
+        dut.clock.step(1)
+      }
+      dut.io.apb.PRDATA.expect(0xaa.U)
+    }
+  }
+}
